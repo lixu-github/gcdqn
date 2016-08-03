f9842b06e13b70df403a0a143310125632f7f69b
dennis-li <lixu_chengdu@163.com>
2016-08-03 10:57:44 +0800

    hi

---
+++ gcdqn/gcdqn/timetable/view/WeekView.h
@@ -0,0 +1,16 @@
+//
+//  WeekView.h
+//  gcdqn
+//
+//  Created by admin on 16/7/19.
+//  Copyright © 2016年 hardtosaygoodbye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TimetableViewController.h"
+
+@interface WeekView : UIView
+
+@property (nonatomic,strong)TimetableViewController *ctrl;
+
+@end
